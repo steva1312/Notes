a200f65eea4f8ef35f4f785975dc98b1411bd7f4
steva1312 <stevanr2005@gmail.com>
2023-06-07 19:58:11 +0200

    trpsonica

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "KURCINA TRPSONSKA", Toast.LENGTH_LONG).show()
+        }
     }
 }
